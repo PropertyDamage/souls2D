cd0ee73e10baa74c74402cea171a236fccef93c9
Pedro <plpires99@gmail.com>
2019-02-26 02:29:06 +0000

    That's all for today
    Added push library so we can get that nice popin' resolution

---
+++ main.lua
@@ -0,0 +1,30 @@
+-- virtual resolution library
+push = require 'push'
+
+virtualWidth = 432
+virtualHeight = 243
+
+windowWidth = 1280
+windowHeight = 720
+
+-- a sprite we will draw and its X-Y coordinates
+local sprite
+local x
+local y
+
+local speed = 10
+
+-- function called at start of game to load assets
+function love.load()
+
+end
+
+-- function called every frame with the delta (dt) since last frame
+function love.update(dt)
+
+end
+
+-- called each frame, used to render to the screen
+function love.draw()
+
+end
